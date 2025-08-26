202f0ddac5ae624829e70758e7ce7c927ca7be8e
Jeff Hodges <jeff@somethingsimilar.com>
2025-08-25 23:25:42 -0700

    put all the things in place for bigquery to be added to deps by gazelle

--- somemain/main.go
+++ somemain/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/bigquery"
+)
 
 func main() {
+	_, err := bigquery.NewClient(context.Background(), "fake-project-id")
+	if err != nil {
+		// do nothing
+	}
 	fmt.Println("Hello, World!")
 }
